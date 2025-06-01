b3879fa35d97cb2ed3bd6e517035fd59b60b689c
Vitor-Azion <vitor.eltz+docs@azion.com>
2025-05-31 23:18:46 -0300

    restart the project

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/work/azion-launch-week.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fazion-launch-week.mdx&astroContentModuleFlag=true")]]);
+		
