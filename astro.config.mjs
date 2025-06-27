bddf65aa5350e84a6017f4f0584ea79780aa68d6
Vitor Adams Eltz <32571274+VitorEltz@users.noreply.github.com>
2025-06-27 19:32:15 -0300

    chore(i18n): add astro-i18next integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import i18next from 'astro-i18next';
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    i18next({
+      defaultLocale: 'en',
+      locales: ['en', 'pt'],
+    }),
+  ],
 });
